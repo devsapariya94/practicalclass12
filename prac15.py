9f237a51d43f716aac1772f898e48f28b8f99da9
Dev Sapariya <66549974+devsapariya94@users.noreply.github.com>
2021-01-11 15:25:08 +0530

    Update prac15.py

--- prac15.py
+++ prac15.py
@@ -1,5 +1,5 @@
 import mysql.connector as sqlcon
-mycon=sqlcon.connect(host="localhost", user="root", password="942003", database="dev")
+mycon=sqlcon.connect(host="localhost", user="root", password="pass", database="test")
 cur=mycon.cursor()
 cur.execute("select * from school")
 data=cur.fetchall()
