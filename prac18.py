9c04de11dac0720b0cdfb22164cd6b76f762848c
Dev Sapariya <66549974+devsapariya94@users.noreply.github.com>
2021-01-11 15:27:06 +0530

    Update prac18.py

--- prac18.py
+++ prac18.py
@@ -1,25 +1,3 @@
-# # Python program to find the k most frequent words 
-# # from data set 
-# from collections import Counter 
-
-# #data_set = "phished emails"  
-# def most_common_word(dataset , n=10):   
-#     # split() returns list of all the words in the string 
-#     split_it = data_set.split() 
-    
-#     # Pass the split_it list to instance of Counter class. 
-#     count = Counter(split_it) 
-    
-#     # most_common() produces k frequently encountered 
-#     # input values and their respective counts. 
-#     most_occur = count.most_common(n) 
-    
-#     print(most_occur)
-# file1=open("first.txt")
-# most_common_word(file1)
-# file1.close()
-
-
 import collections
 fin = open('email.txt','r')
 a= fin.read()
@@ -41,8 +19,6 @@ for k in L:
            count=L.count(key)
            d[key]=count
 
-# n_print = int(input("How many most common words to print: "))
-# print("\nOK. The {} most common words are as follows\n".format(n_print))
 print( "top 5 common word are: ")
 word_counter = collections.Counter(d)
 for word, count in word_counter.most_common(5):
